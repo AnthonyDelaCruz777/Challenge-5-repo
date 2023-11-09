5636a74f2f2058e6bd75e7869fb7e8c6c882169d
AnthonyDelaCruz777 <b00149504@mytudublin.ie>
2023-11-09 11:31:34 +0000

    Challenge 5

---
+++ Challenge 5/Assets/Challenge 5/Scripts/linkScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linkScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenChannel(){
+        Application.OpenURL("https://github.com/AnthonyDelaCruz777/Challenge-5-repo");
+    }
+    
+}
